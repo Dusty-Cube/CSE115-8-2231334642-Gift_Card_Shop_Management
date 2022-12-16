7a7ce832baed2dd5d0cdf479e0115c34a8ee43c1
Dusty-Cube <nasim.rahaman@northsouth.edu>
2022-12-16 12:50:22 +0600

    Adding "main.c" & "splash.h"
    A splash screen function has been called in the main body. It shows some icons related to my project, for a short amount of time.

---
+++ Project 0/SplashScreen.h
@@ -0,0 +1,48 @@
+void splash(void)
+{
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("  				 ^P##P!.                                              .~?5GGBBGPY7^.                                 .~J5PGGGGGP5?~.                                \n");
+    printf("  				J@&7B@@@&5^                                       .?B&&B5?!~^^~7JG#&&5^                           ~P#B57^..  ...^!YB#P^                             \n");
+    printf("  				B@Y  !@@@#&@#J:                                 ^B@#?.              ~P@&?                       Y&B!.           .....!B&J                           \n");
+    printf("  				B@Y   .G@#^:J#@@B7.                           .B@G.            .:^!.   7@@!                   7@G.           ?BG5YYPG5:.B@!                         \n");
+    printf("  				B@Y     !@@J   ~P&@&P~.                      ^@@:       7YGB####B#@B     P@P                 G@~           ^@B::?YY!.~&P !@5                        \n");
+    printf("  				B@Y       G@&:    J@@@@&Y^                  :@&        ^@&!^^::^~5@B      J@P               P@:            @B Y@?^^B@. @5 ^@Y                       \n");
+    printf("  				B@Y        ~@@5  G@&: :?#@@#?.              &@:        ^@@G####BGB@B       B@~             ^@7            !@7 #@.  ?@^ @G  J@:                      \n");
+    printf("  				B@Y          P@&@@J       ^Y&@P            :@#         ^@&^..    :@B       ~@G             P@.           5@7   JBGGP^.B@.  .@Y                      \n");
+    printf("  				B@Y          .&@@#          7@@.           ~@G         :@B     .~G@B       ^@#             7BBGPJ~: ^JPG&#.     :??JG&5     @P                      \n");
+    printf("  				B@Y         !@@J5@@^    .7B@@B^            .@&       :!G@B   J&&BB@B       ?@5              ...:!JPB#?:..     7##Y7!:      ^@7                      \n");
+    printf("  				B@Y       .B@#.  :&@P~P&@&P~                G@7    Y&&GP@B  :@@. ?@G       @@.             .&@BPJ!:      ^! 7@G^           #&                       \n");
+    printf("  				B@Y      7@@?    :P@@@G!.                    &@!  :@@. ?@G   ?&&&&5.      #@!               ^@G.^J@5   ~7@P B@            G@:                       \n");
+    printf("  				B@Y    .#@B. :?#@@B?:                         B@5  ~#&&&Y               ~@@~                 :&#. !@5..JJ~:P@!          :&&.                        \n");
+    printf("  				B@Y   ?@@5!G@@&Y^                              !&@Y.                  ~#@P                     5@P..JGGPPGBJ.         :G&Y                          \n");
+    printf("  				B@P .#@@@@&P~.                                   !#@#J^.          :7G&&Y.                       .J#B?^..          .^Y##?                            \n");
+    printf("  				:&@&@@&G7.                                         .!P#&&#BGPGGB#&&B?:                             .?PBBP5JJ?JY5GBBP7.                              \n");
+    printf("  				  :~~:                                                  .:^~!!~^.                                       .^~!!!~^.                                   \n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("  				                               .:.                                             .^!7???7!^.                                                          \n");
+    printf("  				                               G@BBGPY7^.                                   .B@&5!^:::^!Y&@B.                                                       \n");
+    printf("  				                               G@  .:~?5GBBG?.                          :PGJ^.:75PJ: .?PP?^.:?G5:                                                   \n");
+    printf("  				                               G@         .^5&P                       .G#~.~5GY:  ^5GP~  .JGP!.~#G.                                                 \n");
+    printf("  				                               G@   ~&B&5    .&&                     !@?      ^5&J     !&G^      J&~                                                \n");
+    printf("  				                               G@   #@ J@.    :@7                   !@^       .GB:  ^. .GB^       ^@~                                               \n");
+    printf("  				                               G@   #& ?@.    .@J                  .@!       5&~ .YB5BP. :#G       ?@                                               \n");
+    printf("  				                               G@   #& J@.    .@J                  Y&      :&P .P#7   ~BG: J&^      @?                                              \n");
+    printf("  				                               G@   #& .&B?777B&:                  BB     !@~.P#~       ^BB.:&J     #G                                              \n");
+    printf("  				                         :!JG~ G@   #&   ^!!!!^                    5&    7@^?&7           ~&Y:&Y    &Y                                              \n");
+    printf("  				                   .^?PBBPJ~:  G@   #&    .^!7???!:.               :@^  ~@7#G               5&!@?  ~@.                                              \n");
+    printf("  				                !GBG57^..:!YG~ G@   #& .GBGJ7!7!7JPBBP^             Y@. &#&?                 ~&B@..@?                                               \n");
+    printf("  				               .@#.    7G57^.  G@   #&  ..:!YPB~    :&@              Y&P@@!                   ^@@#&J                                                \n");
+    printf("  				                :5BBG5J??7JPB~ G@.  #& .GG5?^..:!YGBB5:               ~&@P                     Y@&^                                                 \n");
+    printf("  				                    .^!77!~:   !GBBG@&  ..^?5GBGY!:                     !BP~.               .!GB~                                                   \n");
+    printf("  				                                   .:^ .BG57:.                            :JGPY!^......:^7YPGJ:                                                     \n");
+    printf("  				                                                                              :!?JY555YJ?~:                                                         \n");
+
+    sleep(5);
+    system("cls");
+
+    return 0;
+}
---
+++ Project 0/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <dos.h>
+#include "SplashScreen.h"
+
+int main(void)
+{
+    splash();
+
+    return 0;
+}
